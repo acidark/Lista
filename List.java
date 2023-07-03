d4349105a8a4bfd19a60091a3230b96c39d47004
acidark <atomikx@gmail.com>
2023-07-03 11:35:09 -0400

    First commit

---
+++ List.java
@@ -0,0 +1,66 @@
+public class List<Type> {
+    private int nextFreeIndex;
+    private int counter;
+    private Type[] lista;
+    public List(){
+        this.lista = (Type[]) new Object[10];
+        this.nextFreeIndex =0;
+        this.counter=0;   
+    };
+    public void add(Type value){
+        if(this.nextFreeIndex==this.lista.length){
+            grow();
+        }
+        this.lista[nextFreeIndex] = value;
+        nextFreeIndex++;
+    }
+    public void grow(){
+        int howMuch = this.lista.length+this.lista.length/2;
+        Type[] newValues = (Type[]) new Object[(howMuch)];
+        for(int i =0;i<this.lista.length;i++){
+            newValues[i] = this.lista[i];
+
+        }
+        this.lista= newValues;
+    }
+    public int found(Type value){
+        for(int i=0;i< this.nextFreeIndex;i++){
+            if(this.lista[i].equals(value)){
+ 
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public boolean contains(Type value){
+        return found(value)>=0;
+    }
+    public void remove(Type value){
+
+        if (found(value)>=0){
+
+            moveToTheLeft(found(value));
+            this.nextFreeIndex--;
+
+        } 
+        return;
+    }
+    public void moveToTheLeft(int index){
+        for(int i=0;i<index+1;i++){
+            this.lista[index]=this.lista[index+1];
+
+        }
+    }
+
+    public int size(){
+        return this.nextFreeIndex;
+    }
+    public Type value(int index){
+        if(index <0 || index >= this.nextFreeIndex){
+            throw new ArrayIndexOutOfBoundsException(this.nextFreeIndex);
+        }
+        return this.lista[index];
+
+    }
+}
---
+++ Program.java
@@ -0,0 +1,41 @@
+
+import java.util.Scanner;
+
+//import javax.sound.sampled.AudioFileFormat.Type;
+//import javax.swing.plaf.FontUIResource;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        // test your class here
+        /* 
+        List<Object> nuevaLista = new List<>();
+        //for(int i=0;i<6;i++){
+        //  nuevaLista.add(i);
+        //}
+        nuevaLista.add(7);
+        nuevaLista.add(8);
+   
+        System.out.println(nuevaLista.found(7));
+        System.out.println(nuevaLista.found(8));
+
+        nuevaLista.remove(7);
+        System.out.println(nuevaLista.found(7));
+        nuevaLista.remove(8);
+        System.out.println(nuevaLista.found(8));*/
+        List<String> nuevaLista2 = new List<>();
+        nuevaLista2.add("hola");
+        nuevaLista2.add("bye");
+        System.out.println(nuevaLista2.size());
+
+        //System.out.println(nuevaLista2.value(3));
+        /*System.out.println(nuevaLista2.contains("hola"));
+        System.out.println(nuevaLista2.contains("bye"));
+        nuevaLista2.remove("hola");
+        nuevaLista2.remove("bye");
+        System.out.println(nuevaLista2.contains("hola"));
+        System.out.println(nuevaLista2.contains("bye"));/* */
+    }
+
+}
