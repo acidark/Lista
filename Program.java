70aea37d54236c62cca4e05a0bc23c4d6b5404ca
acidark <atomikx@gmail.com>
2023-07-03 12:16:07 -0400

    emergency fix

--- Program.java
+++ Program.java
@@ -29,6 +29,7 @@ public class Program {
         nuevaLista2.add("bye");
         System.out.println(nuevaLista2.size());
         System.out.println("test");
+        System.out.println("emergency fix");
 
         //System.out.println(nuevaLista2.value(3));
         /*System.out.println(nuevaLista2.contains("hola"));
