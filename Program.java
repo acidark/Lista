c30b2da5bb1e01996909310a0c43a368aed3ece7
acidark <atomikx@gmail.com>
2023-07-03 13:25:17 -0400

    ??

--- Program.java
+++ Program.java
@@ -29,11 +29,8 @@ public class Program {
         nuevaLista2.add("bye");
         System.out.println(nuevaLista2.size());
         System.out.println("test");
-<<<<<<< HEAD
         System.out.println("emergency fix");
-=======
         System.out.println("test2 with img");
->>>>>>> test
 
         //System.out.println(nuevaLista2.value(3));
         /*System.out.println(nuevaLista2.contains("hola"));
